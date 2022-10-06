70546a793300e4b3081e08484a6aadd0c2d3aa04
Aleksandar <alexandar.radovanovic2712@gmail.com>
2022-10-06 13:10:40 +0200

    JS varijable scope

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
 
 
+function forLoop() {
+    for (var i = 0; i < 10; i++) {
+        let element = i;
+        console.log(element);
+    }
+    console.log(outsideFunction);
+
+
+    var insideFunction = 9;
+    console.log(insideFunction);
+}
+ 
+    
+forLoop();
+
+    var outsideFunction = 5;
+//U konsoli se prikaze samo poslednja iteracija varijable element
+
+//desi se greska jer let varijabla ima function scope
+
+//dobijemo undefined u consoli
+
+//ispise se vrednost definisane varijable
