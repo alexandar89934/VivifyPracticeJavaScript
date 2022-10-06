a331909ced7e11261d8386ab5bef669c7cab7cf5
Aleksandar <alexandar.radovanovic2712@gmail.com>
2022-10-06 13:40:14 +0200

    JS Arrays

--- main.js
+++ main.js
@@ -23,3 +23,39 @@ forLoop();
 //dobijemo undefined u consoli
 
 //ispise se vrednost definisane varijable
+
+
+const colorArray = ['red','green','blue'];
+
+for (let i = 0; i < colorArray.length; i++) {
+    const element = colorArray[i];
+    console.log(element);
+    
+}
+
+colorArray.forEach(element => {
+    console.log(element);   
+});
+
+
+
+var myZoo = [
+    ["King Kong", ["gorilla", 42]],
+    ["Nemo", ["fish", 5]],
+    ["Punxsutawney Phil", ["groundhog", 11]]
+  ];
+
+  function zooInventory(array) {
+    var output = [];
+    for (var i = 0; i < array.length; i++) {
+      output.push(array[i][0] + ' the ' + array[i][1][0] + ' is ' + array[i][1][1]);
+    }
+    return output;
+  }
+  
+  console.log(zooInventory(myZoo));
+  
+      
+        
+    
+
